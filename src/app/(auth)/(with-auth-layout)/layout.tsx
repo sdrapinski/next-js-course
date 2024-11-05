9b5a3e1af69d3996c4f471c55e75f5cc9815be7f
sdrapinski <szymondrapinski@gmail.com>
2024-11-05 02:17:04 +0100

    usePathname

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,14 +1,35 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
+
+const navLinks = [
+  {name:"Register",href:"/register"},
+  {name:"Login",href:"/login"},
+  {name:"Forgot Password",href:"/forgot-password"}
+]
 
 export default function AuthLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+
+    const pathname = usePathname()
+    
     return (
       
         <div>
-          <h1>Inner layout</h1>
+          {
+            navLinks.map((link)=>{
+              const isActive = pathname.startsWith(link.href)
+              return (
+                <Link href={link.href} key={link.name} className={isActive?"font-bold mr-4" : "text-blue-500 mr-4"}>
+                  {link.name}
+                </Link>
+              )
+            })
+          }
           {children}
         </div>
     
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     
-    <h1> Home</h1>
+    <div>
+      <h1>Home</h1>
+      <Link href="/blog"> Blog </Link>
+    </div>
   );
 }
